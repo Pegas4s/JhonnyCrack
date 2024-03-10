20e3681397a14a6ebca1dcbca611ac612d738b95
Justuxs <90310266+Justuxs@users.noreply.github.com>
2024-03-10 10:28:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,24 @@ const API_KEY =
 const provider = new ethers.JsonRpcProvider(API_KEY);
 
 async function main() {
+  let i = 1;
   while (1) {
     let wallet = ethers.Wallet.createRandom(); // create a new wallet
     let mnemonic = wallet.mnemonic.phrase; // mnemonic
     let address = wallet.address; // address
     let balance = ethers.formatEther(await provider.getBalance(wallet.address)); // balance
-    console.log(balance);
+    if (i % 500 === 0) {
+        console.log(balance); // Log the balance
+        i = 0;
+    }
 
+    i++;
     if (balance !== "0.0") {
+      console.log(mnemonic);
+      console.log(balance);
       console.log(wallet);
+      console.log(address);
+
       // found ether in wallet
       let crackedData;
       await fs
